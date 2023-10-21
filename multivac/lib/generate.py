5b06d4506c584690ca7d65637e9fa78a0d91ae3a
ml5885 <44534695+ml5885@users.noreply.github.com>
2023-10-21 17:09:36 -0400

    Added image generation using Replicate

--- multivac/lib/generate.py
+++ multivac/lib/generate.py
@@ -3,7 +3,19 @@ from langchain.schema import (
     AIMessage,
     HumanMessage
 )
-from .model import GPT
-from .prompt import (
+from multivac.lib.model import GPT
+from multivac.lib.prompt import (
     SYSTEM_TEMPLATE,
 )
+import replicate
+
+def generate_image(prompt, width=512, height=512):
+    model = "doriandarko/sdxl-hiroshinagai:563a66acc0b39e5308e8372bed42504731b7fec3bc21f2fcbea413398690f3ec"
+    input = {
+        "prompt": prompt,
+        "width": width,
+        "height": height,
+    }
+    output = replicate.run(model,input=input)
+    url = output[0]
+    return url
--- multivac/lib/model.py
+++ multivac/lib/model.py
@@ -2,4 +2,4 @@ from langchain.chat_models import ChatOpenAI, ChatAnthropic
 from ..config import OPENAI_API_KEY
 
 GPT = ChatOpenAI(openai_api_key=OPENAI_API_KEY, model="gpt-4")
-CLAUDE = ChatAnthropic()
+# CLAUDE = ChatAnthropic()
