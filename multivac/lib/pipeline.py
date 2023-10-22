8b5f1c9ad6e9587676d86276cc4bcc7a5f37e166
ml5885 <44534695+ml5885@users.noreply.github.com>
2023-10-22 00:19:13 -0400

    added part of generate_settings pipeline

--- multivac/lib/pipeline.py
+++ multivac/lib/pipeline.py
@@ -1,4 +1,15 @@
 from datetime import datetime
+from .generate import setting
+from multivac.db import Book
 
 def extract_states():
     pass
+
+def generate_settings(uuid):
+    book = Book.query(uuid)
+    if not book:
+        return "Book does not exist"
+    book = Book(**book)
+    _, index = book.get_chat()
+    settings = setting(index)
+    return settings.settings
